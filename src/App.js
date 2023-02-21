9afd1633a446acd930f655fde1f69ec94ca77912
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-21 19:32:27 +0600

    form set and design it and validate it and seend email formsubmit.co

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LineGradient from "./components/LineGradient";
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
 import Testimonials from "./scenes/Testimonials";
+import Contact from "./scenes/Contact";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -39,7 +40,7 @@ function App() {
         <Landing setSelectedPage={setSelectedPage} />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
+      <div className="w-5/6 mx-auto">
         <MySkills />
       </div>
       <LineGradient />
@@ -47,8 +48,12 @@ function App() {
         <Projects />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
+      <div className="w-5/6 mx-auto">
         <Testimonials />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Contact />
       </div>
     </div>
   );
