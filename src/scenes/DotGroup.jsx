a4f54752067f1356bb1e248b5064e1176de609c2
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-21 09:08:43 +0600

    set navbar menu and design for mobile screens and set hamburg menu and its childs

---
+++ src/scenes/DotGroup.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const DotGroup = ({ selectedPage, setSelectedPage }) => {
+  return <div className="flex flex-col gap-6 top-[60%] right-7">DotGroup</div>;
+};
+
+export default DotGroup;
